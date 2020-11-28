81e10b4ba1ad6df82ee04246a2b1b2ab4cae97cd
codekissyoung <1162097842@qq.com>
2020-11-28 19:14:03 +0800

    继续学习erlang

--- 20201120/src/linkmon.erl
+++ 20201120/src/linkmon.erl
@@ -1,19 +1,63 @@
 -module(linkmon).
 -author("cky").
--export([chain/1, myproc/0]).
+-compile([debug_info, export_all]). % 编译选项: 加入调试信息，导出全部函数
 
 myproc() ->
   timer:sleep(5000),
-  throw({no_reason}).
+  exit(no_reason).
 
 chain(0) ->
   receive
     _ -> ok
-  after 2000 ->
-    throw("chain dies here")
+  after 3000 ->
+    exit("chain dies here")
   end;
 chain(N) ->
-  link(spawn( fun() -> chain(N - 1) end )),
+  spawn_link( fun() -> chain(N - 1) end ),
   receive
     _ -> ok
   end.
+
+judge(Pid ,Band) ->
+  Pid ! {self(), {Band}},
+  receive
+    {Pid, Criticism}
+      -> Criticism
+  after 2000 ->
+    timeout
+  end.
+
+start_critic() ->
+  spawn(?MODULE, critic, []).
+
+start_critic2() ->
+  spawn(?MODULE, protect_critic, []).
+
+judge2(X) ->
+  critic ! {self(),X},
+  Pid = whereis(critic),
+  receive
+    {Pid, Y} -> Y
+  after 2000 ->
+    timeout
+  end.
+
+protect_critic() ->
+  process_flag(trap_exit, true),
+  Pid = spawn_link(?MODULE, critic, []),
+  register(critic, Pid),                  % 给进程命名, 挂到注册树上
+  receive
+    {'EXIT', Pid, normal} -> ok;          % exit normal
+    {'EXIT', Pid, shutdown} -> ok;        % exit by hand
+    {'EXIT', Pid, _} -> protect_critic()  % restart it
+  end.
+
+critic() ->
+  receive
+    {From, {"a"}}           -> From ! {self(),"A"};
+    {From, {"b"}}           -> From ! {self(),"B"};
+    {From, {X}}             -> From ! {self(),X}, X;
+    {From, {_Band, _Album}} -> From ! {self(),_Band, _Album}
+  end,
+  critic().
+
