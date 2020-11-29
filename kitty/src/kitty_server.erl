1319c84b0cf33b86de620ce19bace1f02df9e4be
codekissyoung <1162097842@qq.com>
2020-11-30 02:25:58 +0800

    init tolist path

--- kitty/src/kitty_server.erl
+++ kitty/src/kitty_server.erl
@@ -1,13 +1,72 @@
-%%%-------------------------------------------------------------------
-%%% @author cky
-%%% @copyright (C) 2020, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 30. 11月 2020 上午1:48
-%%%-------------------------------------------------------------------
 -module(kitty_server).
--author("cky").
+-compile(export_all).
+-record(cat, {name, color=green, description}).
 
-%% API
--export([]).
+order_cat(Pid, Name, Color, Desc) ->
+  Ref = erlang:monitor(process,Pid),
+  Pid ! {self(), Ref, {order, Name, Color, Desc}},
+  receive
+    {Ref, Cat} ->
+      erlang:demonitor(Ref, [flush]),
+      Cat;
+    {'DOWN', Ref, process, Pid, Reason } ->
+      erlang:error(Reason)
+    after 5000 ->
+    erlang:error(timeout)
+  end.
+
+return_cat(Pid, Cat = #cat{}) ->
+  Pid ! {return, Cat},
+  ok.
+
+close_shop(Pid) ->
+  Ref = erlang:monitor(process,Pid),
+  Pid ! {self(), Ref, terminate},
+  receive
+    {Ref, ok} ->
+      erlang:demonitor(Ref,[flush]),
+      ok;
+    {'DOWN', Ref, process, Pid, Reason } ->
+      erlang:error(Reason)
+  after 5000 ->
+    erlang:error(timeout)
+  end.
+
+start_link()
+  -> spawn_link(fun init/0).
+
+init() ->
+  loop([]).
+
+loop(Cats) ->
+  receive
+
+    {Pid, Ref, {order, Name, Color, Desc}} ->
+      if
+        Cats =:= [] ->
+          Pid ! {Ref, make_cat(Name,Color,Desc)},
+          loop(Cats);
+        Cats =/= [] ->
+          Pid ! {Ref, hd(Cats)},
+          loop(tl(Cats))
+      end;
+
+    {return,Cat = #cat{}} ->
+      loop([Cat|Cats]);
+
+    {Pid, Ref, terminate} ->
+      Pid ! {Ref, ok},
+      terminate(Cats);
+
+    Unknown ->
+      io:format("Unknown message : ~p~n",[Unknown]),
+      loop(Cats)
+
+  end.
+
+make_cat(Name, Col, Desc) ->
+  #cat{name=Name, color=Col, description = Desc}.
+
+terminate(Cats) ->
+  [io:format("~p was set free. ~n", [C#cat.name]) || C <- Cats ],
+  ok.
